8409dfc488f3ebf5381303e28d9eb410571485ce
sandm <1424193282@qq.com>
2018-07-01 22:01:16 +0800

    C和C++输入输出的验证

---
+++ basic/cout_cin.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+////printf的使用
+//int main()
+//{
+//    int a =10;
+//    float d = 2.333;
+//    printf("%d,%lf\n",a,d);
+//
+//    scanf("%d %lf",&a,&d);
+//    printf("%d,%lf\n",a,d);
+//    return 0;
+//}
+
+
+int main()
+{
+    int a = 10;
+    double d = 2.233233;
+    cout<<a<<endl;
+    cout<<d<<endl;
+
+    cin>>a>>d;
+    cout<<a<<endl;
+    cout<<d<<endl;
+
+    return 0;
+}
