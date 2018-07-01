52e99b548a872b2d2b7b3cdb7585a8f1ba2af2b5
sandm <1424193282@qq.com>
2018-07-01 21:28:41 +0800

    C++基础入门

---
+++ basic/first.cpp
@@ -0,0 +1,60 @@
+
+
+//名字空间和域作用解析符的使用
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+//int a = 30;
+namespace my
+{
+    int a = 10;
+    namespace it
+    {
+        int a = 50;
+    }
+}
+namespace your
+{
+    int a = 20;
+}
+
+//int a = 30;
+//using namespace your;
+using namespace my;
+int a = 30;
+//cout<<a<<endl;
+int main()
+{
+    int a = 40;
+    //std::cout<<a<<std::endl;
+    //std::cout<<::a<<std::endl;
+    //std::cout<<my::a<<std::endl;
+    //std::cout<<your::a<<std::endl;
+
+    cout<<a<<endl;
+    cout<<::a<<endl;
+    cout<<my::it::a<<endl;
+    cout<<your::a<<endl;
+    return 0;
+}
+
+
+
+//int a = 10;
+//int main()
+//{
+//    int a = 20;
+//    //printf和cout的输出均遵循就近原则
+//    //::左边为空，表示访问的是全局域
+//    printf("%d\n",::a);
+//    cout<<::a<<endl;
+//    return 0;
+//}
+
+//int main()
+//{
+//    std::cout<<"hello world!"<<std::endl;
+//    return 0;
+//}
